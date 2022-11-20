98045ddbe0bd39a95b7e783a156546eeba74c63d
david van duzer <dvd@tennica.net>
2022-11-20 00:57:12 -0700

    dvd.tennica.net hello world

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
+  greeting = "<h1>dvd.tennica.net checking in</h1>";
   name = req.params["name"];
   if (name) {
-    res.send(greeting + "</br>and hello to " + name);
+    res.send(greeting + '</br>and hello to <a rel="me"' + 'href="https://tilde.zone/' + name + '">' + name + '</a>' );
   } else {
-    res.send(greeting);
+    res.send('<h1><a rel="me" href="https://tilde.zone/@dvd">dvd.tennica.net</a> is now verified</a>');
   }
 });
 
